88bb47802f280c2f9e5d91c6a83fdeb4b2e51be7
Adnan Assad <adnnn336@gmail.com>
2025-09-23 08:25:03 +0600

    Ready for deployment

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_h(umc^w2#uzip=b&c@mo9l-if-4mcon8jwbp^jc2eq3_e_y$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", '127.0.0.1']
 AUTH_USER_MODEL = 'users.User'
